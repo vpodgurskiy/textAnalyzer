f3d888dc4cce9348a237eb22c2e464e0dd9c6bde
vpodgurskiy <vpodgurskiy@gmail.com>
2018-07-01 08:21:32 +0300

    #3

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,11 @@ import model.QueryParameters;
 import outputer.ProcessingTheResult;
 import parser.InputStreamParser;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.printf("Text analyzer >_ ");
-        Scanner in = new Scanner(System.in);
-        String inputStream = in.nextLine();
+        String inputStream = String.join(" ", args);
 
         final QueryParameters queryStringParameters = InputStreamParser.parseInputStream(inputStream);
 
